e42cdcea6d0da9627693b2a0f9ecbb8f6a03dfed
Glenn Kasten <gkasten@google.com>
2014-01-20 09:16:22 -0800

    Build tinyplay for host
    Change-Id: I6e4c00e50850fb3bdaa2df82186e494003b3226d

--- Android.mk
+++ Android.mk
@@ -30,6 +30,15 @@ include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
 LOCAL_C_INCLUDES:= external/tinyalsa/include
+LOCAL_SRC_FILES:= tinyplay.c
+LOCAL_MODULE := tinyplay
+LOCAL_STATIC_LIBRARIES:= libcutils libutils libtinyalsa
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_HOST_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_C_INCLUDES:= external/tinyalsa/include
 LOCAL_SRC_FILES:= tinycap.c
 LOCAL_MODULE := tinycap
 LOCAL_SHARED_LIBRARIES:= libcutils libutils libtinyalsa
