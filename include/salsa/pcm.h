b9ff2bf7ede1c04b571a15fd3f8068b2d3d00bf4
Taylor Holberton <taylorcholberton@gmail.com>
2016-05-22 00:45:58 -0400

    fixed close prototype and added open prototype

--- include/salsa/pcm.h
+++ include/salsa/pcm.h
@@ -1,13 +1,20 @@
 #ifndef SALSA_PCM_H
 #define SALSA_PCM_H
 
+typedef enum salsa_pcm_stream {
+	SALSA_PCM_STREAM_PLAYBACK = 0,
+	SALSA_PCM_STREAM_CAPTURE,
+} salsa_pcm_stream;
+
 typedef struct salsa_pcm {
 	int fd;
 } salsa_pcm;
 
 void salsa_pcm_init(salsa_pcm * pcm);
 
-void salsa_pcm_free(salsa_pcm * pcm);
+void salsa_pcm_close(salsa_pcm * pcm);
+
+int salsa_pcm_open(salsa_pcm * pcm, unsigned card, unsigned device, salsa_pcm_stream);
 
 int salsa_pcm_get_file_descriptor(const salsa_pcm * pcm);
 
