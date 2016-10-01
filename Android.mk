db29593a2f658d571f4d01ab869c3e11f8f413fe
Taylor Holberton <taylorcholberton@gmail.com>
2016-10-01 12:37:13 -0400

    updated program paths

--- Android.mk
+++ Android.mk
@@ -12,7 +12,7 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_C_INCLUDES:= external/tinyalsa/include
-LOCAL_SRC_FILES:= tinyplay.c
+LOCAL_SRC_FILES:= utils/tinyplay.c
 LOCAL_MODULE := tinyplay
 LOCAL_SHARED_LIBRARIES:= libcutils libutils libtinyalsa
 LOCAL_MODULE_TAGS := optional
@@ -21,7 +21,7 @@ include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
 LOCAL_C_INCLUDES:= external/tinyalsa/include
-LOCAL_SRC_FILES:= tinywavinfo.c
+LOCAL_SRC_FILES:= utils/tinywavinfo.c
 LOCAL_MODULE := tinywavinfo
 LOCAL_SHARED_LIBRARIES:= libcutils libutils libm
 LOCAL_MODULE_TAGS := optional
@@ -30,7 +30,7 @@ include $(BUILD_EXECUTABLE)
 ifeq ($(HOST_OS), linux)
 include $(CLEAR_VARS)
 LOCAL_C_INCLUDES:= external/tinyalsa/include
-LOCAL_SRC_FILES:= tinywavinfo.c
+LOCAL_SRC_FILES:= utils/tinywavinfo.c
 LOCAL_MODULE := tinywavinfo
 LOCAL_STATIC_LIBRARIES:= libcutils libutils
 LOCAL_MODULE_TAGS := optional
@@ -39,7 +39,7 @@ endif
 
 include $(CLEAR_VARS)
 LOCAL_C_INCLUDES:= external/tinyalsa/include
-LOCAL_SRC_FILES:= tinycap.c
+LOCAL_SRC_FILES:= utils/tinycap.c
 LOCAL_MODULE := tinycap
 LOCAL_SHARED_LIBRARIES:= libcutils libutils libtinyalsa
 LOCAL_MODULE_TAGS := optional
@@ -48,7 +48,7 @@ include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
 LOCAL_C_INCLUDES:= external/tinyalsa/include
-LOCAL_SRC_FILES:= tinymix.c
+LOCAL_SRC_FILES:= utils/tinymix.c
 LOCAL_MODULE := tinymix
 LOCAL_SHARED_LIBRARIES:= libcutils libutils libtinyalsa
 LOCAL_MODULE_TAGS := optional
@@ -57,7 +57,7 @@ include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
 LOCAL_C_INCLUDES:= external/tinyalsa/include
-LOCAL_SRC_FILES:= tinypcminfo.c
+LOCAL_SRC_FILES:= utils/tinypcminfo.c
 LOCAL_MODULE := tinypcminfo
 LOCAL_SHARED_LIBRARIES:= libcutils libutils libtinyalsa
 LOCAL_MODULE_TAGS := optional
