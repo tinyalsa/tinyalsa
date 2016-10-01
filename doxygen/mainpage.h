1e21aed210e9e1e1bb3383423c21799a93de2064
Taylor Holberton <taylorcholberton@gmail.com>
2016-10-01 15:17:33 -0400

    Added main page for doxygen

---
+++ doxygen/mainpage.h
@@ -0,0 +1,10 @@
+/** @mainpage Preamble
+ *
+ * Welcome to the documentation for the TinyALSA project.
+ * <br><br>
+ * To start, you may either view the @ref pcm or @ref mixer.
+ * <br><br>
+ * If you find an error in the documentation or an area for improvement,
+ * open an issue or send a pull request to the <a href="https://github.com/tinyalsa/tinyalsa">github page</a>.
+ */
+
