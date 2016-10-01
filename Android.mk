582fce5f72b72b0425a623a53f5a070feeeae904
Taylor Holberton <taylorcholberton@gmail.com>
2016-10-01 13:09:12 -0400

    updated library source paths

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_C_INCLUDES:= external/tinyalsa/include
-LOCAL_SRC_FILES:= mixer.c pcm.c
+LOCAL_SRC_FILES:= src/mixer.c src/pcm.c
 LOCAL_MODULE := libtinyalsa
 LOCAL_SHARED_LIBRARIES:= libcutils libutils
 LOCAL_MODULE_TAGS := optional
