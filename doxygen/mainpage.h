38298e6c569b3f5c0ed53cf250cf26f5bb8279fe
Taylor Holberton <taylorcholberton@gmail.com>
2016-11-19 23:45:07 -0500

    updated references

--- doxygen/mainpage.h
+++ doxygen/mainpage.h
@@ -2,7 +2,7 @@
  *
  * Welcome to the documentation for the TinyALSA project.
  * <br><br>
- * To start, you may either view the @ref tinyalsa-pcm or @ref tinyalsa-mixer.
+ * To start, you may either view the @ref libtinyalsa-pcm or @ref libtinyalsa-mixer.
  * <br><br>
  * If you find an error in the documentation or an area for improvement,
  * open an issue or send a pull request to the <a href="https://github.com/tinyalsa/tinyalsa">github page</a>.
