e125261297a6c33b6f0f441bd446d79dfc7c5f9d
Jaikrishna Nemallapudi <jaikrishnax.nemallapudi@intel.com>
2017-04-12 11:16:29 +0530

    Fix to pass num_frames to pcm_writei
    pcm_writei expects size of data in frames, whereas currently size
    is passed as number of bytes. So convert number of bytes to frames
    for argument to pcm_writei.
    
    Signed-off-by: Jaikrishna Nemallapudi <jaikrishnax.nemallapudi@intel.com>
    Signed-off-by: Subhransu S. Prusty <subhransu.s.prusty@intel.com>

--- utils/tinyplay.c
+++ utils/tinyplay.c
@@ -409,7 +409,8 @@ int play_sample(struct ctx *ctx)
     do {
         num_read = fread(buffer, 1, size, ctx->file);
         if (num_read > 0) {
-            if (pcm_writei(ctx->pcm, buffer, num_read) < 0) {
+		if (pcm_writei(ctx->pcm, buffer,
+			pcm_bytes_to_frames(ctx->pcm, num_read)) < 0) {
                 fprintf(stderr, "error playing sample\n");
                 break;
             }
